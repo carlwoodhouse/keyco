4e6a357f13daa8b6d4a79f079a20d19d4f419751
Carl Woodhouse <carl.woodhouse@gmail.com>
2024-01-31 20:46:09 +0000

    lu env

--- components/header/headerBanner.jsx
+++ components/header/headerBanner.jsx
@@ -4,7 +4,7 @@ const GUILD_NAME = process.env.NEXT_PUBLIC_GUILD_NAME;
 class HeaderBanner extends Component  {
     render() {
         return (
-            <img src={ "/" + GUILD_NAME + ".webp" } className="img-fluid" alt="..."></img>
+            <img src={ "/" + GUILD_NAME.replace(" ",  "").toLowerCase() + ".webp" } className="img-fluid" alt="..."></img>
         )
     }
 }
