c88a57ef54ec7f4547ca79148720e061d511ecd5
Carl Woodhouse <carl.woodhouse@gmail.com>
2024-07-09 23:31:20 +0100

    fix some buff things

--- components/roster/buffs.jsx
+++ components/roster/buffs.jsx
@@ -4,7 +4,7 @@ import * as Icon from 'react-bootstrap-icons';
 class RosterBuff extends Component {
     render() {
         let buffCount = this.props.raiders.filter(x=> x.class == this.props.buffClass).length ?? 0;
-        return (<tr className={this.props.buffClass.toLowerCase()}>
+        return (<tr className={this.props.buffClass.toLowerCase().replace(' ', '')}>
                     <td className='class'>{this.props.buffDescription}</td>         
                     {buffCount == 0 ? <td><Icon.X color='red' /></td> : <td><Icon.Check2 color='green' /></td>}
                     {buffCount == 0 ? <td>&nbsp;</td> : <td>x{buffCount}</td>}
@@ -28,10 +28,11 @@ class RosterBuffs extends Component  {
                 <RosterBuff raiders={this.props.raiders} buffClass="Druid" buffDescription="3% Vers" />
                 <RosterBuff raiders={this.props.raiders} buffClass="Warrior" buffDescription="5% Attack Power" />
                 <RosterBuff raiders={this.props.raiders} buffClass="Demon Hunter" buffDescription="5% Magic Damage" />
-                <RosterBuff raiders={this.props.raiders} buffClass="Monk" buffDescription="5% Physical Dmg" />
+                <RosterBuff raiders={this.props.raiders} buffClass="Monk" buffDescription="5% Physical Damage" />
                 <RosterBuff raiders={this.props.raiders} buffClass="Shaman" buffDescription="Skyfury (Autoattack / 2% Mastery)" />
                 <RosterBuff raiders={this.props.raiders} buffClass="Paladin" buffDescription="Damage Reduction" />
                 <RosterBuff raiders={this.props.raiders} buffClass="Rogue" buffDescription="Damage Reduction Chance (Poison)" />
+                <RosterBuff raiders={this.props.raiders} buffClass="Hunter" buffDescription="5% Damage over 80% health" />
             </tbody>
         </table>)
     }
