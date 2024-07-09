5ef5afffb9e3344364ccd7a44ecf74342608e0b6
Carl Woodhouse <carl.woodhouse@gmail.com>
2024-07-10 00:07:27 +0100

    one more sluggy

--- components/guildLinks/raiderioLink.jsx
+++ components/guildLinks/raiderioLink.jsx
@@ -1,8 +1,9 @@
 import ToolLink from "../externalTools/toolLink"
+import { slug } from "../../utils";
 
 class RaiderIOLink extends ToolLink {
     render() {
-        return <ToolLink className="d-inline p-right" title="RaiderIO Profile" icon="rio.svg" href={"https://raider.io/guilds/eu/" + this.props.realm + "/" + this.props.name} />;
+        return <ToolLink className="d-inline p-right" title="RaiderIO Profile" icon="rio.svg" href={"https://raider.io/guilds/eu/" + slug(this.props.realm) + "/" + this.props.name} />;
     }
 }
 
