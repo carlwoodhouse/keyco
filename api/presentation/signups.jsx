d8b50e540f5cc9a0686acaea2c6359075b23c469
Carl Woodhouse <carl.woodhouse@gmail.com>
2024-07-10 16:52:39 +0100

    sorting for nath

--- api/presentation/signups.jsx
+++ api/presentation/signups.jsx
@@ -4,12 +4,37 @@ import SignupCharacter from "../../components/character/signupCharacter";
 export default class presentationSignups {
     constructor() {}
 
+   static characterRoleCompare(a, b) {
+        if (a.role > b.role) {
+            return -1;
+        }
+
+        if (a.role < b.role) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    static characterNameCompare(a, b) {
+        if (a.name < b.name) {
+            return -1;
+        }
+
+        if (a.name > b.name) {
+            return 1;
+        }
+
+        return 0;
+    }
+
     static async buildRosterTree() {
         if (process.env.SIGNUP_ENABLED !== "1") {
             return [];
         }
 
         const sheetData = await googleSheetsClient.getSheetData(process.env.SIGNUP_SHEET_ID, 'A7:K50');
-        return sheetData.sort().filter(x => x[0] !== '').map((row) => new SignupCharacter(row));
+        const signups = sheetData.filter(x => x[0] !== '').map((row) => new SignupCharacter(row));
+        return signups.sort(this.characterNameCompare).sort(this.characterRoleCompare);
     } 
 }
