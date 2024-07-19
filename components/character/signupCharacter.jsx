ab51bba8fec4d12e07bcb486ed99733d4bd7235d
Carl Woodhouse <carl.woodhouse@gmail.com>
2024-07-19 20:46:39 +0100

    make trial/limited text more robust

--- components/character/signupCharacter.jsx
+++ components/character/signupCharacter.jsx
@@ -3,8 +3,8 @@ class SignupCharacter {
     this.name = character[0];
     this.class = character[1];
     this.role = character[2];
-    this.trial = character[10].indexOf("Trial") != -1;
-    this.limited = character[10].indexOf("Limited") != -1;
+    this.trial = character[10] !== undefined && character[10].indexOf("Trial") != -1;
+    this.limited = character[10] !== undefined && character[10].indexOf("Limited") != -1;
   }
 }
 
