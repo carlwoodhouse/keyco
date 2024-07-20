4fa3665d48fe2167b36eaedc84c439942e4a9ec5
Carl Woodhouse <carl.woodhouse@gmail.com>
2024-07-20 23:27:37 +0100

    role enforcement in signups

--- components/character/signupCharacter.jsx
+++ components/character/signupCharacter.jsx
@@ -2,7 +2,15 @@ class SignupCharacter {
   constructor(character) {
     this.name = character[0];
     this.class = character[1];
-    this.role = character[2];
+
+    let role = character[2];
+    let validRoles = ["TANK", "HEALER", "DPS"];
+    
+    if (!validRoles.includes(role)) {
+      role = "DPS";
+    } 
+
+    this.role = role;
     this.trial = character[10] !== undefined && character[10].indexOf("Trial") != -1;
     this.limited = character[10] !== undefined && character[10].indexOf("Limited") != -1;
   }
